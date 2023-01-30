1b09b0618d07ee5aba56ae896c2e1415868694e1
Piatrouski Herman <herman.piatrouski@generali.com>
2023-01-30 23:46:07 +0100

    start creating a component

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-    
+    if (size === 0) return '';
+      
+    if (size === undefined) return string;
+
+    const firstSlice = string.slice(0, size);
+    const rest = [...string.slice(size)];
+  
+    return rest.reduce((str, char) => {
+      if (!str.endsWith(char.repeat(size))) {
+        str += char;
+      }
+  
+      return str;
+    }, firstSlice);
 }
--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,91 @@
 export default class ColumnChart {
+  chartHeight = 50;
 
+  constructor({
+      data = [],
+      label = '',
+      value = '',
+      link = ''
+  }) {
+      this.data = data;
+      this.label = label;
+      this.value = value;
+      this.link = link;
+      this.render();
+  }
+
+  render() {
+      const element = document.createElement('div');
+      element.innerHTML = this.createElements();
+      this.element = element.firstElementChild;
+  }
+
+  createElements() {
+    return `
+    <div class="dashboard__chart_orders">
+      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.getBalance()}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.createColumn(this.data)}
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  getBalance(sales) {
+    sales = this.value;
+    if(sales > 20000) {
+      let fixedBalance = '' + sales;
+      let res = fixedBalance.slice(0,3)
+      let ressum = fixedBalance.slice(3, 6)
+
+      return `$${res}.${ressum}`
+    }
+
+    return `
+      ${sales}
+    `
+  }
+
+  createColumn(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+    .map(item => {
+      const percent = (item / maxValue * 100).toFixed(0);
+
+      return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+    })
+    .join('');
+  }
+
+  update(data) {
+    this.subElements.body.innerHTML = this.getColumnBody(data);
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+    
 }
